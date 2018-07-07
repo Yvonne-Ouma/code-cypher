d0f1bad8aae6e7500164e073f933a245b5a0e36e
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-07-07 11:01:04 +0300

    prompt working

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-alert("hi there")
+// alert("hi there");
+/*
+prompt.
+store the answer to the prompt in a variable.
+print it to the console.
+*/
+
+prompt("Enter a sentence");
+
+ var sentence=prompt();
+ console.log(sentence)
