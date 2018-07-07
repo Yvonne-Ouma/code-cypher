0455dda40c9e84924cc4fbc756ee60887e26ccbc
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-07-07 11:35:53 +0300

    added prompt. working

--- scripts.js
+++ scripts.js
@@ -8,4 +8,19 @@ print it to the console.
 prompt("Enter a sentence");
 
  var sentence=prompt();
- console.log(sentence)
+ console.log(sentence);
+ var firstletter=sentence.charAt();
+ console.log(firstletter);
+ var lastletter=sentence.charAt(sentence.length-1);
+ console.log(lastletter.toUpperCase());
+
+ /*
+ create a function.
+ get first letter.
+ capitalize first letter.
+ get last letter.
+ capitalize.
+ replace the initial first
+replace the initial last.
+return the new string.
+*/
